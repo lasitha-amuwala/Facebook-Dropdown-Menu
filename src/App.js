3b7a2886270a3ec48257862a0db500077b8ceee1
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-08-30 17:35:13 -0400

    Added classNames and menu animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import classNames from 'classnames';
+
 import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as BoltIcon } from './icons/bolt.svg';
@@ -12,33 +15,12 @@ const App = () => {
   return (
     <Navbar>
       <NavItem icon={<PlusIcon />} />
-      <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<BellIcon />} />
       <NavItem icon={<CaretIcon />}>
         <DropdownMenu />
       </NavItem>
     </Navbar>
-  );
-};
-
-const DropdownMenu = () => {
-    
-  const DropdownItem = ({ children, leftIcon, rightIcon }) => (
-    <a href="#" className="menu-item">
-      <span className="icon-button">{leftIcon}</span>
-      {children}
-      <span className="icon-right">{rightIcon}</span>
-    </a>
-  );
-
-  return (
-    <div className="dropdown">
-      <DropdownItem>My Profile</DropdownItem>
-      <DropdownItem
-        leftIcon={<CogIcon />}
-        rightIcon={<ChevronIcon />}
-      ></DropdownItem>
-    </div>
   );
 };
 
@@ -53,15 +35,67 @@ const Navbar = ({ children }) => {
 const NavItem = ({ icon, children }) => {
   const [open, setOpen] = useState(false);
 
-  const handleOpenClose = () => setOpen(!open);
-
   return (
     <li className="nav-irem">
-      <a href="#" className="icon-button" onClick={handleOpenClose}>
+        <a href="#" className={classNames('icon-button', { 'active' : open })} onClick={() => setOpen(!open)}>
         {icon}
       </a>
       {open && children}
     </li>
   );
 };
+
+const DropdownMenu = () => {
+  const [activeMenu, setActiveMenu] = useState('main');
+
+  const DropdownItem = ({ children, leftIcon, rightIcon, goToMenu }) => (
+    <a
+      href="#"
+      className="menu-item"
+      onClick={() => goToMenu && setActiveMenu(goToMenu)}
+    >
+      <span className="icon-button">{leftIcon}</span>
+      {children}
+      <span className="icon-right">{rightIcon}</span>
+    </a>
+  );
+
+  return (
+    <div className="dropdown">
+      <CSSTransition
+        in={activeMenu === 'main'}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >
+            Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
+          <DropdownItem>CSS</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
 export default App;
